84c3308303d92c100325497b2ed2f4ad33259a0b
Fernando Rojo <frojo@sas.upenn.edu>
2023-08-06 13:26:24 -0400

    remove tst

--- test/read.ts
+++
@@ -1,8 +0,0 @@
-import { loadTamaguiSync } from "@tamagui/static"
-
-const config = loadTamaguiSync({
-  components: ["tamagui"],
-  config: "./test/tamagui.config.ts",
-})
-
-console.log("[config]", config)
