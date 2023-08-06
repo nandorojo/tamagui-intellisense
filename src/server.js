2a518819332c6a678e4afeb028d96f60a8cb4ca6
Fernando Rojo <frojo@sas.upenn.edu>
2023-08-06 13:16:12 -0400

    plugin

--- src/server.js
+++ src/server.js
@@ -35,23 +35,8 @@ const getTamaguiConfig = (
   return config
 }
 
-connection.onInitialize((params) => {
-  getTamaguiConfig(params)
-
-  return {
-    capabilities: {
-      textDocumentSync: TextDocumentSyncKind.Full,
-      completionProvider: {
-        resolveProvider: true,
-        triggerCharacters: ["$"],
-      },
-    },
-  }
-})
-
-connection.onCompletion((params) => {
+const getCompletionItems = (params) => {
   const tamaguiConfig = getTamaguiConfig(params)
-
   /**
    * @type {CompletionItem[]}
    */
@@ -59,25 +44,25 @@ connection.onCompletion((params) => {
   if (tamaguiConfig) {
     const { tokens, themes } = tamaguiConfig
 
-    if (tokens) {
-      autocompleteItems.push(
-        ...Object.entries(tokens.color).map(([key, token]) => {
-          const name = `$${key}`
+    // if (tokens) {
+    //   autocompleteItems.push(
+    //     ...Object.entries(tokens.color).map(([key, token]) => {
+    //       const name = `$${key}`
 
-          const item = CompletionItem.create(name)
+    //       const item = CompletionItem.create(name)
 
-          console.log("[token]", name, token)
+    //       console.log("[token]", name, token)
 
-          item.kind = CompletionItemKind.Color // Use Text instead of Color
-          item.documentation = [
-            color.hsl(token.val).hex(),
-            ["AKA", token.val].join(" "),
-          ].join("\n")
+    //       item.kind = CompletionItemKind.Color // Use Text instead of Color
+    //       item.documentation = [
+    //         color.hsl(token.val).hex(),
+    //         ["AKA", token.val].join(" "),
+    //       ].join("\n")
 
-          return item
-        })
-      )
-    }
+    //       return item
+    //     })
+    //   )
+    // }
 
     if (themes) {
       const [firstTheme] = Object.values(themes)
@@ -102,20 +87,30 @@ connection.onCompletion((params) => {
 
             const item = CompletionItem.create(name)
 
-            console.log("[theme]", name, theme)
-
             item.kind = CompletionItemKind.Color // Use Text instead of Color
             item.documentation = {
               kind: "markdown",
               value: allColors
                 .map(([themeName, hsl]) => {
-                  return [
-                    `![${themeName}](https://tamagui-intellisense.vercel.app//api/image-color?color=${hsl})`,
-                    themeName,
-                  ].join(" ")
+                  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><rect x="1" y="1" width="22" height="22" fill="${hsl}" rx="4" /></svg>`
+
+                  // ignore components like dark_Button
+                  const ignorePatterh = /_[A-Z]/
+
+                  if (ignorePatterh.test(themeName)) {
+                    return ``
+                  }
+
+                  return `![Image](data:image/svg+xml;base64,${btoa(
+                    svg
+                  )}) **${themeName.replace(/_/g, " ")}**`
                 })
-                .join("<br>"),
+                // TODO single loop to improve perf
+                .filter(Boolean)
+                .join("\n<br>"),
             }
+
+            console.log("[theme]", item.documentation.value)
 
             return item
           })
@@ -125,6 +120,28 @@ connection.onCompletion((params) => {
   }
 
   return autocompleteItems
+}
+
+let completionItems = []
+
+connection.onInitialize((params) => {
+  getTamaguiConfig(params)
+
+  completionItems = getCompletionItems(params)
+
+  return {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Full,
+      completionProvider: {
+        resolveProvider: true,
+        triggerCharacters: ["$"],
+      },
+    },
+  }
+})
+
+connection.onCompletion((params) => {
+  return completionItems
 })
 
 // onCompletionResolve handler
